d791b331cdc3f9c71717e2c27a6b8c3bdf928ae9
pmb087 <pmb087@gmail.com>
2022-08-29 15:32:36 +0900

    Initial commit

---
+++ src/Router.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Nav />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function Nav() {
+  return <h1>Nav 입니다.</h1>;
+}
+export default Nav;
